61ef752a8a92b916eea6eaa739ebaccb194b696a
Shreshth Purohit <shreshthpurohit@gmail.com>
2025-01-31 22:21:00 +0530

    fix type errors

--- server/src/routes/StudentRoute.ts
+++ server/src/routes/StudentRoute.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 const router = express.Router();
 const prisma = new PrismaClient();
+import {Request, Response} from 'express'
 
-router.get('/', async (req, res) => {
+router.get('/', async (req: Request, res: Response) => {
   try {
     const students = await prisma.student.findMany({
       select: {
