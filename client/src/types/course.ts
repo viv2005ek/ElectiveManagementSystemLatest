46d181a9eafe53ecf149ef6995b0a92b8c481006
raajshekhar19 <raajshekharbgp@gmail.com>
2025-02-04 10:44:10 +0530

    programme elective page done ig

---
+++ client/src/types/course.ts
@@ -0,0 +1,15 @@
+export interface Course {
+    id: string
+    courseCode: string
+    name: string
+    semester: number
+    isStandalone: boolean
+    minorSpecializationId: string
+  }
+  
+  export interface PreferenceSelection {
+    preference: number
+    course: Course
+  }
+  
+  
