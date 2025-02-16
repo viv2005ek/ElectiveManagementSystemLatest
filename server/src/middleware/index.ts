be0c1e21cd4e8a1db6538bedd8b4a268d34c28be
Shreshth Purohit <shreshthpurohit@gmail.com>
2025-02-16 14:48:09 +0530

    cors config

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -4,7 +4,16 @@ import cookieParser from "cookie-parser";
 import express, { Express } from "express";
 
 export const setupMiddleware = (app: Express) => {
-  app.use(cors({ origin: "*", credentials: true }));
+  import cors from "cors";
+
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:3000",
+      credentials: true, // Allow cookies
+      allowedHeaders: ["Content-Type", "Authorization"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(morgan(process.env.NODE_ENV === "production" ? "tiny" : "dev"));
