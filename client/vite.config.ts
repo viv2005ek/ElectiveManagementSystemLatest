8aa91665340fca902b7cc66b05bf5e665f9a4a8c
Shreshth Purohit <shreshthpurohit@gmail.com>
2025-02-23 18:08:12 +0530

    made it a pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "MUJ Elective Allocation System",
+          short_name: "MUJ EMS",
+          description: "App for managing elective management system",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+        },
+      }),
+    ],
   server: {
     port: 3000,
   },
